a7cfca734d7b65619b31d2e97d78bc30f15610c0
Tạ Hữu Đạt <tahuudat16032004@gmail.com>
2025-11-02 13:30:31 +0700

    Gen cơ bản giao diện gen imgae/audio/video

--- frontend/src/components/GenAudio.jsx
+++ frontend/src/components/GenAudio.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
+import { Headphones } from 'lucide-react';
 
 const GenAudio = ({story, setSelectedOption}) => {
   return (
-  <div className="m-7 text-white w-full min-h-screen">
-      <button
-        onClick={ () => setSelectedOption(null) }
-        className="cursor-pointer mt-4 bg-purple-700 px-4 py-2 rounded-md text-white hover:bg-purple-800"
-      >
-        ← Quay lại
-      </button>
+   <div className="mr-20 text-white w-full min-h-153 border border-purple-500/20 rounded-md p-5 space-y-6">
+      <div className='flex justify-between items-center border border-purple-500/20 p-5 px-13 rounded-md'>
+        <div className='flex flex-row  items-center '>
+          <Headphones className="inline-block w-6 h-6 mr-2 text-blue-400" />
+          <h2 className="text-2xl font-bold ">Generate Audio</h2>
+        </div>
+
+        <button
+          onClick={ () => setSelectedOption(null) }
+          className="cursor-pointer bg-purple-700 px-3 py-2 rounded-md text-white hover:bg-purple-800"
+        >
+          ← Back to input
+        </button>
+      </div>
+      <div className="border border-purple-500/20 p-4 px-13 rounded-md h-116">
+        content coming soon...
+      </div>
     </div>
   )
 }
